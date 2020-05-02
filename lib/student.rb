6faa59abfd5eb538643972c1339a63351e67189b
AHMED AFANDY <eng.computer1@yahoo.com>
2020-05-02 16:47:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -80,17 +80,17 @@ class Student
 
  def self.first_X_students_in_grade_10(num_stud)
    sql=<<-SQL
-      SELECT * FROM students WHERE grade = 10
+      SELECT * FROM students WHERE grade = 10 LIMIT ?
    SQL
     arr=[]
     a=0
-      DB[:conn].execute(sql).map { |e|
-        while a < num_stud
-          arr<<self.new_from_db(e)
-          a += 1
-        end
+      DB[:conn].execute(sql,num_stud).map { |e|
+      #  while a < num_stud
+      #    arr<<self.new_from_db(e)
+      #    a += 1
+      #  end
         }
-      arr
+    #  arr
  end
 
  def self.first_student_in_grade_10
